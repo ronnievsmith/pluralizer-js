1687715d32b2909d1199ccbb93c19e05fb7524b1
Ron Royston <ron@rack.pub>
2017-09-04 22:35:58 +0000

    abracadabra

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
-# [pluralizer.js](http://rack.pub/pluralizer.min.js)
+# pluralizer.js
 ### Synopsis
 
-Pluralizer (5.48 KB) takes data in the form of quantity, item name and returns the plural form of item name if quantity is 2 or more.  pluralizer.js returns 2 public methods - read and format, and one public property, help. 
+Pluralizer (7.19 KB) returns the plural form of whatever word you give it.  It can also take an array of string arrays, each having a quantity and corresponding word, and return well formed english with an Oxford comma. See code examples below. Pluralizer.js returns 2 public methods - read and run, and one public property, help. 
 
 ### Code Example
 ```
 pluralizer.read([[2,'orange'],[3,'peach'],[5,'cherry']]) --> '2 oranges, 3 peaches, and 5 cherries.'.
-pluralizer.format([3,'couch']) --> '[3, 'couches']'
-pluralizer.help --> "pluralizer.js returns 2 public methods - read and format.  pluralizer.read expects an array of arrays, each with quantity and item name, e.g. pluralizer.read([[2,'orange'],[3,'peach'],[5,'cherry']]) returns string '2 oranges, 3 peaches, and 5 cherries.'.  pluralizer.format expects an array with quantity and item name, e.g., pluralizer.format([3,'couch']) returns array '[3, 'couches']'"
+pluralizer.run('goose') --> 'geese'
+pluralizer.help --> "pluralizer.js returns 2 public methods - read and run.  pluralizer.read expects an array of arrays, each with quantity and item name, e.g. pluralizer.read([[2,'orange'],[3,'peach'],[5,'cherry']]) returns string '2 oranges, 3 peaches, and 5 cherries.'.  pluralizer.run expects a string, e.g., pluralizer.run('goose') returns 'geese'."
 ```
 > :apple: read returns a string with an Oxford comma in arrays of 3 or more items.
 
@@ -27,8 +27,8 @@ I needed to automate a summary of input items.  Items with qty 2 and up need to 
 
 ```
 pluralizer.read([[2,'orange'],[3,'peach'],[5,'cherry']]) --> '2 oranges, 3 peaches, and 5 cherries.'.
-pluralizer.format([3,'couch']) --> '[3, 'couches']'
-pluralizer.help --> "pluralizer.js returns 2 public methods - read and format.  pluralizer.read expects an array of arrays, each with quantity and item name, e.g. pluralizer.read([[2,'orange'],[3,'peach'],[5,'cherry']]) returns string '2 oranges, 3 peaches, and 5 cherries.'.  pluralizer.format expects an array with quantity and item name, e.g., pluralizer.format([3,'couch']) returns array '[3, 'couches']'"
+pluralizer.run('goose') --> 'geese'
+pluralizer.help --> "pluralizer.js returns 2 public methods - read and run.  pluralizer.read expects an array of arrays, each with quantity and item name, e.g. pluralizer.read([[2,'orange'],[3,'peach'],[5,'cherry']]) returns string '2 oranges, 3 peaches, and 5 cherries.'.  pluralizer.run expects a string, e.g., pluralizer.run('goose') returns 'geese'."
 ```
 
 ### Contribute
--- pluralizer.js
+++ pluralizer.js
@@ -1,17 +1,18 @@
 /*
-pluralizer.js v0.1 - Add-on tool for JavaScript that returns plural form of English words
+pluralizer.js v1.1 - Add-on tool for JavaScript that returns plural form of English words
 Created by Ron Royston, https://rack.pub
 https://github.com/rhroyston/pluralizer-js
 License: MIT
 */
 
-var testArray = [[2,'orange'],[3,'peach'],[5,'cherry']];
+
 
 //Revealing Module Pattern (Public & Private) w Public Namespace 'pluralizer'
 var pluralizer = (function() {
     var pub = {};
     var r = 'pluralizer.js error';
-    var expectedArrayOfArrays = {name:r, message:'Invalid argument.  Expected array of arrays'};
+    var expectedArrayOfArrays = {name:r, message:"Invalid argument.  Expected array of arrays"};
+    var expectedString = {name:r, message:"Invalid argument.  Expected string"};
     //creates Array.isArray() if it's not natively available
     if (!Array.isArray) {
         Array.isArray = function(arg) {
@@ -163,9 +164,6 @@ var pluralizer = (function() {
         'vespers'];
     var doc = document;
 
-    doc.addEventListener("DOMContentLoaded", function(event) {
-        
-    });
     pub.help = "pluralizer.js returns 2 public methods - read and format.  pluralizer.read expects an array of arrays, each with quantity and item name, e.g. pluralizer.read([[2,'orange'],[3,'peach'],[5,'cherry']]) returns string '2 oranges, 3 peaches, and 5 cherries.'.  pluralizer.format expects an array with quantity and item name, e.g., pluralizer.format([3,'couch']) returns array '[3, 'couches']'"
     pub.read = function (arr) {
         if(isArrayOfArrays(arr)){
@@ -205,10 +203,194 @@ var pluralizer = (function() {
             }
             return str;
         } else {
-            throw expectedArrayOfArrays;
+            try {
+                throw new Error('pluralizer.read had an issue processing argument ');
+            }
+            catch (e) {
+                console.log(e.name + ': ' + e.message);
+            }
         }
-    }
+    };
     
+    pub.run = function(str){
+        if(typeof str === "string"){
+            //Word ends in s, x, ch, z, or sh
+            if (str.endsWith('s') || str.endsWith('x') || str.endsWith('ch') || str.endsWith('sh') || str.endsWith('z')){
+                //look for exceptions first xExceptions
+                for (var i = 0; i < xExceptions.length; i++) {
+                    if(str === xExceptions[i][0]){
+                        return xExceptions[i][1];
+                    }
+                }
+                //str = str.substring(0, str.length - 1);
+                str = str + 'es';
+                return str;
+            }
+            // Ending in 'y'
+            else if (str.endsWith('y')){
+                var s = str.substring(0, str.length - 1);
+                // preceded by a vowel
+                if (s.endsWith('a') || s.endsWith('e') || s.endsWith('i') || s.endsWith('o') || s.endsWith('u')){
+                    str = str + 's';
+                    return str;
+                } else {
+                    //drop the y and add ies
+                    str = s + 'ies';
+                    return str;
+                }
+            }
+            //Ends with 'ff' or 'ffe'
+            else if (str.endsWith('ff') || str.endsWith('ffe')){
+                str = str + 's';
+                return str;
+            }
+            //Ends with 'f' (but not 'ff')
+            else if (str.endsWith('f')){
+                //look for exceptions first fExceptions
+                for (var i = 0; i < fExceptions.length; i++) {
+                    if(str === fExceptions[i][0]){
+                        return fExceptions[i][1];
+                    }
+                }
+                //Change the 'f' to 'ves'
+                var s = str.substring(0, str.length - 1);
+                str = s + 'ves';
+                return str;
+            }
+            //Ends with 'fe' (but not ffe')
+            else if (str.endsWith('fe')){
+                //look for exceptions first feExceptions
+                for (var i = 0; i < feExceptions.length; i++) {
+                    if(str === feExceptions[i][0]){
+                        return feExceptions[i][1];
+                    }
+                }
+                //Change the 'fe' to 'ves'
+                var s = str.substring(0, str.length - 2);
+                str = s + 'ves';
+                return str;
+            }
+            //Ends with 'o'
+            else if (str.endsWith('o')){
+                //look for exceptions first oExceptions
+                for (var i = 0; i < oExceptions.length; i++) {
+                    if(str === oExceptions[i][0]){
+                        return oExceptions[i][1];
+                    }
+                }
+                //Add 'es'
+                str = s + 'es';
+                return str;
+            }
+            //Ends with 'is'
+            else if (str.endsWith('is')){
+                //Change final 'is' to 'es'
+                var s = str.substring(0, str.length - 2);
+                str = s + 'es';
+                return str;
+            }
+            //Ends with 'us'
+            else if (str.endsWith('us')){
+                //look for exceptions first oExceptions
+                for (var i = 0; i < usExceptions.length; i++) {
+                    if(str === usExceptions[i][0]){
+                        return usExceptions[i][1];
+                    }
+                }
+                //Change final 'us' to 'i'
+                var s = str.substring(0, str.length - 2);
+                str = s + 'i';
+                return str;
+            }
+            //Ends with 'um'
+            else if (str.endsWith('um')){
+                //look for exceptions first oExceptions
+                for (var i = 0; i < umExceptions.length; i++) {
+                    if(str === umExceptions[i][0]){
+                        return umExceptions[i][1];
+                    }
+                }
+                //Change final 'um' to 'a'
+                var s = str.substring(0, str.length - 2);
+                str = s + 'a';
+                return str;
+            }  
+            //Ends with 'a' but not 'ia'  
+            else if (str.endsWith('a')){
+                //not ending is 'ia'
+                if (str.endsWith('ia')){
+                    str = str + 's';
+                    return str;
+                }
+                //look for exceptions first aExceptions
+                for (var i = 0; i < aExceptions.length; i++) {
+                    if(str === aExceptions[i][0]){
+                        return aExceptions[i][1];
+                    }
+                }
+                //Change final 'a' to 'ae'
+                var s = str.substring(0, str.length - 2);
+                str = s + 'a';
+                return str;
+            }                 
+            //Ends with 'on'  Change final 'on' to 'a'
+            else if (str.endsWith('on')){
+                //look for exceptions first onExceptions
+                for (var i = 0; i < onExceptions.length; i++) {
+                    if(str === onExceptions[i][0]){
+                        return onExceptions[i][1];
+                    }
+                }
+                //Change final 'um' to 'a'
+                var s = str.substring(0, str.length - 2);
+                str = s + 'a';
+                return str;
+            }
+            //Ends with 'ex'
+            else if (str.endsWith('ex')){
+                //look for exceptions first onExceptions
+                for (var i = 0; i < exExceptions.length; i++) {
+                    if(str === exExceptions[i][0]){
+                        return exExceptions[i][1];
+                    }
+                }
+                //Change final 'ex' to 'ices'
+                var s = str.substring(0, str.length - 2);
+                str = s + 'ices';
+                return str;
+            }
+            else {
+                //check unchanging
+                for (var i = 0; i < unchanging.length; i++) {
+                    if(str === unchanging[i]){
+                        return str;
+                    }
+                }                    
+                //check onlyPlurals
+                for (var i = 0; i < onlyPlurals.length; i++) {
+                    if(str === onlyPlurals[i]){
+                        return str;
+                    }
+                }
+                //check irregular
+                for (var i = 0; i < irregular.length; i++) {
+                    if(str === irregular[i][0]){
+                        return irregular[i][1];
+                    }
+                }
+                str = str + 's';
+                return str;
+            }
+        } else {
+            try {
+                throw new Error('pluralizer.run had an issue processing argument ');
+            }
+            catch (e) {
+                console.log(e.name + ': ' + e.message);
+            }
+        }        
+    };
+ 
     pub.format = function (arr) {
         //if qty is greater than 1 we need to add s, es, or ies
         var qty = arr[0];
@@ -384,21 +566,18 @@ var pluralizer = (function() {
         } else {
             return [qty,str];
         }
-    }
-    
+    }   
     function isArrayOfArrays(arr){
         if(Array.isArray(arr)){
             var result = true;
             for (var i = 0; i < arr.length; i++) {
                 if(!Array.isArray(arr[i])){
                     result = false;
-                    //throw expectedArrayOfArrays;
                 }
             }
             if(result){
                 return true;
             } else {
-                //throw expectedArrayOfArrays;
                 return false;
             }
         } else {
